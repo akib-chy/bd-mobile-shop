76d1c32a2c3b9a22cda260c1dbe8987227f206b4
Akib Chy <akibchy1212@gmail.com>
2022-03-01 20:18:37 +0600

    html and js added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+// REUSE COMPONENT
+const itemNoneAlert1 = document.getElementById("danger1");
+const itemNoneAlert2 = document.getElementById("danger2");
+const phoneDetails = document.getElementById("phone-details");
+const phoneItem = document.getElementById("phone-item");
+
 // SPINER START AND END ADDED
 const startSpiner = (spiner) => {
   document.getElementById("spiner").style.display = spiner;
@@ -10,9 +16,12 @@ const loadPhones = () => {
 
   const searchInputText = searchInput.value;
   searchInput.value = "";
-  if (searchInputText === "") {
+  if (searchInputText === "" || searchInputText === !Number) {
+    itemNoneAlert1.style.display = "none";
+    itemNoneAlert2.style.display = "block";
     startSpiner("none");
-    return alert("hello");
+    phoneItem.textContent = "";
+    return itemNoneAlert2;
   }
   fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchInputText}`
@@ -24,27 +33,27 @@ const loadPhones = () => {
 // SHOW DISPLAY PHONE DATA
 
 const displayPhones = (phones) => {
-  const phoneDetails = document.getElementById("phone-details");
-  const phoneItem = document.getElementById("phone-item");
   const phoneSlice = phones.slice(0, 20);
-  const itemNoneAlert = document.getElementById("danger1");
+
   if (phones.length === 0) {
     phoneDetails.textContent = "";
     phoneItem.textContent = "";
     startSpiner("none");
-    itemNoneAlert.style.display = "block";
-    return itemNoneAlert;
+    itemNoneAlert1.style.display = "block";
+    itemNoneAlert2.style.display = "none";
+    return itemNoneAlert1;
   } else {
     document.getElementById("show-more").style.display = "block";
-    itemNoneAlert.style.display = "none";
+    itemNoneAlert1.style.display = "none";
+    itemNoneAlert2.style.display = "none";
     phoneDetails.textContent = "";
     phoneItem.textContent = "";
     for (const phone of phoneSlice) {
       const div = document.createElement("div");
       div.classList = "col";
-      div.innerHTML = ` <div class="col">
+      div.innerHTML = ` <div class="col shadow p-3 bg-body">
                         <div class="card h-100">
-                      <img src="${phone.image}" class="card-img-top" alt="..." />
+                      <img src="${phone.image}" class="card-img-top w-75 mt-4 mx-auto " alt="..." />
                         <div class="card-body">
                       <h5 class="card-title">${phone.phone_name}</h5>
                        <p class="card-text">${phone.brand}</p>
@@ -85,16 +94,87 @@ const loadPhonesDetails = (detail) => {
     .then((res) => res.json())
     .then((data) => displayPhoneDetails(data));
 };
-
+// ADDED PHONE FOR DISPLAY
 const displayPhoneDetails = (phoneDetail) => {
-  console.log(phoneDetail);
   const phone = phoneDetail.data;
-  const phoneDetails = document.getElementById("phone-details");
+  phoneDetails.textContent = "";
+  window.scrollTo(0, 200);
   const div = document.createElement("div");
-  // const phoneOther = phone.others;
   div.classList = "col-md-8 mb-3 rounded";
-  div.style.maxWidth = "540px";
-  /* if (phone.others === undefined) {
+  div.style.maxWidth = "700px";
+  div.innerHTML = `
+  <div class="card h-100 mt-5">
+    <img src="${
+      phone.image
+    }" class="card-img-top w-50 mx-auto mt-4" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title text-center mt-3 mb-3 ">${
+        phone.name
+      } Full Specifications</h5>
+      <table class="table card-text table-striped table-hover">
+  <tbody class="border">
+    <tr class="border">
+      <th class="border-end">Name</th>
+      <td>${phone.name}</td>
+      </tr>
+    <tr class="border">
+      <th class="border-end">Brand</th>
+      <td>${phone.brand}</td>
+      </tr>
+    <tr class="border">
+      <th class="border-end">First Release</th>
+      <td>${phone.releaseDate || "No Relese Date"}</td>
+      </tr>
+      <tr class="border">
+      <td> Connectivity</td>
+      </tr>
+    <tr class="border">
+      <th class="border-end">Display Size</th>
+      <td >${phone.mainFeatures.displaySize}</td>
+      </tr>
+    <tr class="border">
+      <th class="border-end">Memory</th>
+      <td >${phone.mainFeatures.memory}</td>
+      </tr>
+    <tr class="border">
+      <th class="border-end">WLAN</th>
+      <td >${phone.others?.WLAN || "No Other Feture"}</td>
+      </tr>
+    <tr class="border">
+      <th class="border-end">Bluetooth</th>
+      <td >${phone.others?.Bluetooth || "No Other Feture"}</td>
+      </tr>
+    <tr class="border">
+      <th class="border-end">GPS</th>
+      <td >${phone.others?.GPS || "No Other Feture"}</td>
+      </tr>
+    <tr class="border">
+      <th class="border-end">NFC</th>
+      <td >${phone.others?.NFC || "No Other Feture"}</td>
+      </tr>
+    <tr class="border">
+      <th class="border-end">Radio</th>
+      <td >${phone.others?.Radio || "No Other Feture"}</td>
+      </tr>
+    <tr class="border">
+      <th class="border-end">USB</th>
+      <td >${phone.others?.USB || "No Other Feture"}</td>
+      </tr>
+      <tr class="border">
+      <th class=""> Sensors</th>
+      </tr>
+  </tbody>
+</table>
+      <div class="d-flex justify-content-center mt-4">
+      <button class="btn btn-warning btn-end">Buy Now</button>
+      </div>
+    </div>
+  </div>`;
+
+  phoneDetails.appendChild(div);
+};
+
+/* if (phone.others === undefined) {
     div.innerHTML = `<div class="card h-100">
     <img src="${phone.image}" class="card-img-top" alt="..." />
     <div class="card-body">
@@ -136,74 +216,4 @@ const displayPhoneDetails = (phoneDetail) => {
     phoneDetails.appendChild(div);
     return;
   } */
-  // const map = phone.mainFeatures.sensors.map(hello);
-  div.innerHTML = `
-  <div class="card h-100">
-    <img src="${phone.image}" class="card-img-top" alt="..." />
-    <div class="card-body">
-      <h5 class="card-title text-center mt-3 mb-3 ">${
-        phone.name
-      } Full Specifications</h5>
-      <table class="table card-text">
-  <tbody class="border">
-    <tr class="border">
-      <th class="border-end">Name</th>
-      <td>${phone.name}</td>
-      </tr>
-    <tr class="border">
-      <th class="border-end">Brand</th>
-      <td>${phone.brand}</td>
-      </tr>
-    <tr class="border">
-      <th class="border-end">First Release</th>
-      <td>${phone.releaseDate || "No Relese Date"}</td>
-      </tr>
-      <tr class="border">
-      <td> Connectivity</td>
-      </tr>
-    <tr class="border">
-      <th class="border-end">Display Size</th>
-      <td >${phone.mainFeatures.displaySize}</td>
-      </tr>
-    <tr class="border">
-      <th class="border-end">Memory</th>
-      <td >${phone.mainFeatures.memory}</td>
-      </tr>
-    <tr class="border">
-      <th class="border-end">WLAN</th>
-      <td >${phone.others?.WLAN || "no"}</td>
-      </tr>
-    <tr class="border">
-      <th class="border-end">Bluetooth</th>
-      <td >${phone.others?.Bluetooth || "no"}</td>
-      </tr>
-    <tr class="border">
-      <th class="border-end">GPS</th>
-      <td >${phone.others?.GPS || "no"}</td>
-      </tr>
-    <tr class="border">
-      <th class="border-end">NFC</th>
-      <td >${phone.others?.NFC || "no"}</td>
-      </tr>
-    <tr class="border">
-      <th class="border-end">Radio</th>
-      <td >${phone.others?.Radio || "no"}</td>
-      </tr>
-    <tr class="border">
-      <th class="border-end">USB</th>
-      <td >${phone.others?.USB || "no"}</td>
-      </tr>
-      <tr class="border">
-      <th class=""> Sensors</th>
-      </tr>
-      <tr class="border">
-      <th class=""> </th>
-      </tr>
-    
-  </tbody>
-</table>
-    </div>
-  </div>`;
-
-  phoneDetails.appendChild(div);
-};
+// const map = phone.mainFeatures.sensors.map(hello);
